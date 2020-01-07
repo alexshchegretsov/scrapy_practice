2d6fa89fc46a43784805f712a286bb3615b0873e
alex_shchegretsov <nydollz77@gmail.com>
2020-01-07 20:26:34 +0300

    recursive call next page

--- crawling/crawling/spiders/book_spider.py
+++ crawling/crawling/spiders/book_spider.py
@@ -9,21 +9,23 @@ star_mapper = {
     "Five": 5
 }
 
-pages = int(input("How many pages do you want to scrape:"))
-
 
 class BookSpiderSpider(scrapy.Spider):
     name = 'book_spider'
     allowed_domains = ['books.toscrape.com']
-    start_urls = ['http://books.toscrape.com/catalogue/page-{}.html'.format(i + 1) for i in range(pages)]
+    start_urls = ['http://books.toscrape.com/catalogue/page-1.html']
 
     def parse(self, response):
         data = {}
         books = response.css("ol.row article.product_pod")
         for book in books:
-            data["title"] = book.css("h3 a::attr(title)").getall()
-            data["price"] = book.css("div.product_price p.price_color::text").getall()
+            data["title"] = book.css("h3 a::attr(title)").get()
+            data["price"] = book.css("div.product_price p.price_color::text").get()
             data["availability"] = book.css("div.product_price p.instock.availability::text").getall()[1].strip()
             text_star = book.css("p::attr(class)").get().split()[-1]
             data["star"] = star_mapper[text_star]
             yield data
+        next_page = response.css("div>ul.pager>li.next>a::attr(href)").get()
+        if next_page:
+            next_url = response.urljoin(next_page)
+            yield response.follow(next_url, callback=self.parse)
