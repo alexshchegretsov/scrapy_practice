8d49019559135ce7a3620360b8bfe47a75908cd6
Alex Shchegretsov <46520984+alexshchegretsov@users.noreply.github.com>
2020-01-14 00:30:41 +0300

    Update items.py

--- crawling/crawling/items.py
+++ crawling/crawling/items.py
@@ -16,3 +16,11 @@ class BookCover(scrapy.Item):
     file_urls = scrapy.Field()
     files = scrapy.Field()
 
+"""
+{'image_urls': ['http://books.toscrape.com/media/cache/0b/bc/0bbcd0a6f4bcd81ccb1049a52736406e.jpg'],
+ 'images': [{'checksum': '4b75678760ac88e4bca9c888da999d97',
+             'path': 'full/27bfbda82c55c895ad6621816856c22b43fb2581.jpg',
+             'url': 'http://books.toscrape.com/media/cache/0b/bc/0bbcd0a6f4bcd81ccb1049a52736406e.jpg'}],
+ 'title': 'Libertarianism for Beginners'}
+
+"""
